7438bd1a0161cd8a4a341aed2d15b5ba271ded91
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-08 13:25:27 -0800

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './Components/Header/Header'
 
 
 function App() {
   return (
     <div className="App">
-    <h1>asdsa</h1>
+    <Header />
     </div>
   );
 }
