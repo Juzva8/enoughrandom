0aa1e9abf5e5ce7895b866dff342ce19228e6149
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-09 14:34:12 -0800

    done for today time to study

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import './Services.css'
+import scissors from '../../Assets/Scissor.png'
 
 function Services() {
     return (
         <div className="services">
-         <div className="services__text">
-             <h1>some text</h1>
-             <h1>some text</h1>
-             <h1>some text</h1>
-             <h1>some text</h1>
-             <h1>some text</h1>
-             <h1>some text</h1>
+         <div className="services__content">
+             <div className="services__content__image">
+                 <img src={scissors} alt="scissors"/>
+             <h3>Hair cut</h3>
+             </div>
+             <div className="services__content__image">
+                 <img src={scissors} alt="scissors"/>
+             <h3>Make up</h3>
+             </div>
+             <div className="services__content__image">
+                 <img src={scissors} alt="scissors"/>
+             <h3>Microblading</h3>
+             </div>
+              <div className="services__content__image">
+                 <img src={scissors} alt="scissors"/>
+             <h3>Shading</h3>
+             </div>
+           
          </div>
         </div>
     )
