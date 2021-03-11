e9c3b98d7fcf0451fd65782b38322caab627ecd7
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-11 15:51:55 -0800

    fixed header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,20 +6,20 @@ import logo from '../../Assets/logo2.png';
 
 function Header() {
     return (
-        <div ClassName="header">
+        <div className="header">
             <Navbar bg="light" expand="lg">
               <a href="/" ClassName="Logo" rel="Home">
-  <img  ClassName = "img" src={logo} alt="Logo" />
+  <img  className = "img" src={logo} alt="Logo" />
   </a>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav ClassName="mr-auto">
+    <Nav className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
       <Nav.Link href="/about">About</Nav.Link>
       <Nav.Link href="/contact">Contact</Nav.Link>
     </Nav>
     <Form inline>
-      <FormControl type="text" placeholder="Search" ClassName="mr-sm-2" />
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-success">Search</Button>
     </Form>
   </Navbar.Collapse>
