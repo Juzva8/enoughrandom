71bfa2a250b73d97273bfba53d608f3ca99c77f9
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-11 16:00:06 -0800

    fixed header to be mobile responsice

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import logo from '../../Assets/logo2.png';
 
 
@@ -8,20 +8,16 @@ function Header() {
     return (
         <div className="header">
             <Navbar bg="light" expand="lg">
-              <a href="/" ClassName="Logo" rel="Home">
-  <img  className = "img" src={logo} alt="Logo" />
-  </a>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
+  <a href="/" className="Logo" rel="Home">
+  <img  className = "img" src={logo} alt="Logo" />
+  </a>
     <Nav className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
       <Nav.Link href="/about">About</Nav.Link>
       <Nav.Link href="/contact">Contact</Nav.Link>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
   </Navbar.Collapse>
 </Navbar>
         </div>
