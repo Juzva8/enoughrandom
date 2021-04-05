91c0f0c52cafbba969da733fadb9f9d49c90136f
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-04-05 15:46:02 -0700

    carousel

--- src/Pages/gallery/gallery.js
+++ src/Pages/gallery/gallery.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import ImageSlider from '../../Components/ImageSlider/ImageSlider'
+import Carousel from 'react-bootstrap/Carousel'
 import './gallery.css'
-// import Carousel from 'react-bootstrap/Carousel'
-// import model_1 from '../../Assets/Gallery/1.jpg'
-// import model_2 from '../../Assets/Gallery/2.jpg'
-// import model_3 from '../../Assets/Gallery/3.jpg'
-// import model_4 from '../../Assets/Gallery/4.jpg'
-// import model_5 from '../../Assets/Gallery/5.jpg'
-// import model_6 from '../../Assets/Gallery/6.jpg'
+import model_1 from '../../Assets/Gallery/1.jpg'
+import model_2 from '../../Assets/Gallery/2.jpg'
+import model_3 from '../../Assets/Gallery/3.jpg'
+import model_4 from '../../Assets/Gallery/4.jpg'
+import model_5 from '../../Assets/Gallery/5.jpg'
+import model_6 from '../../Assets/Gallery/6.jpg'
+
+
+
 
 function gallery() {
     return (
         <div className="gallery">
+    
+          <div className="gallery_carousel">
           <h2>PHOTO GALLERY</h2>
-          <ImageSlider />
-            {/* <Carousel fade>
+            <Carousel fade>
   <Carousel.Item>
     <img
       className="gallery__img"
-      src={hair1}
+      src={model_1}
       alt="First slide"
     />
     <Carousel.Caption>
@@ -28,7 +31,7 @@ function gallery() {
   <Carousel.Item>
     <img
       className="gallery__img"
-      src={hair2}
+      src={model_2}
       alt="Second slide"
     />
 
@@ -39,15 +42,47 @@ function gallery() {
   <Carousel.Item>
     <img
       className="gallery__img"
-      src={hair3}
+      src={model_3}
       alt="Third slide"
     />
-
     <Carousel.Caption>
       <h3>Third slide label</h3>
     </Carousel.Caption>
   </Carousel.Item>
-</Carousel> */}
+
+  <Carousel.Item>
+    <img
+      className="gallery__img"
+      src={model_4}
+      alt="Fourth  slide"
+    />
+    <Carousel.Caption>
+      <h3>Fourth slide label</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="gallery__img"
+      src={model_5}
+      alt="Fifth slide"
+    />
+    <Carousel.Caption>
+      <h3>Fifth slide label</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="gallery__img"
+      src={model_6}
+      alt="Sixth  slide"
+    />
+    <Carousel.Caption>
+      <h3>Sixth slide label</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+</Carousel>
+</div>
 </div>
 
             
