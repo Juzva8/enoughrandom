16d1ab7b6c668cafefd7ad4c086ec484bbf17b59
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-04-07 01:13:09 -0700

    almost done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import './Home.css'
-
+import Typewriter from 'typewriter-effect';
 
 function Home() {
     return (
         <div className="home">
-         <div className="home__content">
-           <p>
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         Lorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip Lorem Lorem ipsum dolor sit amet, consectetur adip
-         
-         </p>
-         </div>
+         <Typewriter 
+         onInit = {(typewriter) => {
+           typewriter
+           .typeString("GET MICROBLADING! ")
+           .pauseFor(2000)
+           .deleteAll()
+           .typeString("LET'S ENHANCE AND HIGHLIGHT YOUR NATURAL BEAUTY!")
+           .start()
+         }}
+         />
         </div>
     )
 }
--- src/Pages/pricing/pricing.js
+++ src/Pages/pricing/pricing.js
@@ -5,7 +5,73 @@ import './pricing.css'
 function pricing() {
     return (
         <div className="pricing">
-             <h1>SERVICES & PRICING</h1>
+           <h1>SERVICES & PRICING</h1>
+    <div className="row">
+    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>Microblading</h3>
+   </div>
+   <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>$245</h3>
+   </div>
+   <div className="pricing__paragraph"><p>Microblading is a permanent procedure that mimics the same size of an eyebrow hair stroke through small incisions made with a microblade. 
+    The purpose of microblading is to enhance what you naturally have, and the result is natural-looking, trendy eyebrows. *NOT recommended for oily, or mature skin.</p></div>
+   </div>
+   <div className="row">
+    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>Microblading + Shading (combo)</h3>
+   </div>
+   <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>$295</h3>
+   </div>
+   <div className="pricing__paragraph"><p>This technique offers the best of both powdered and hair stroke effects combined together to achieve a fluffy, 
+       textured, yet defined brow. </p></div>
+   </div>
+   <div className="row">
+    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>Powder Brows</h3>
+   </div>
+   <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>$295</h3>
+   </div>
+   <div className="pricing__paragraph"><p>Powder brows can be done as light as a brow tint or as dark as glam. 
+       Recommended for those who want a completely "filled in" brow! Best for ALL skin types. </p></div>
+   </div>
+   <h1>Touch ups</h1>
+   <div className="pricing__touchup">
+  <h6><strong>Touch ups also make an essential part of permanent makeup treatment. You can lose 30% to 60% of the color and intensity as the skin heals. 
+      Touch up procedures are to fill, correct, and darken to provide the desired look.</strong></h6>
+  </div>
+   <div className="row">
+    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>Touch up</h3>
+   </div>
+   <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>$100</h3>
+   </div>
+   <div className="pricing__paragraph"><p>1 to 3 months after the first session 
+    If you do not come back for a touch up during 3 months period, you will need to pay a full price again.</p></div>
+   </div>
+   <div className="row">
+    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>Annual Touch up</h3>
+   </div>
+   <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>$150</h3>
+   </div>
+   <div className="pricing__paragraph"><p>12 months after touch up session</p></div>
+   </div>
+   <div className="row">
+    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>Biannual touch up</h3>
+   </div>
+   <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
+    <h3>$200</h3>
+   </div>
+   <div className="pricing__paragraph"><p>24 months after touch up session. If you do not come back for a biannual touch up you will need to pay a full price again. </p></div>
+   </div>
+   <div className="pricing__touchup">
+  <h6><strong>*A $50 NON-REFUNDABLE BOOKING FEE IS REQUIRED TO RESERVE YOUR APPOINTMENT. THIS FEE WILL GO TOWARDS YOUR BROWS PRICING.</strong></h6>
+  </div>
         </div>
     )
 }
