0cbc176e096b7a14a017a4c48b293f647bba6b24
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-04-13 11:07:21 -0700

    fixed slider to be mobile responsive

--- src/Pages/gallery/gallery.js
+++ src/Pages/gallery/gallery.js
@@ -1,8 +1,6 @@
 import React from "react";
 import './gallery.css'
 import SliderComponent from '../../Components/Slider/Slider'
-// import Carousel1 from '../../Components/Carousel/Carousel'
-
 
 
 
@@ -11,7 +9,6 @@ function gallery() {
       <div className="gallery">
 <div className="gallery__text"> <h2>PHOTO GALLERY</h2></div>
         <SliderComponent />
-        {/* <Carousel1 /> */}
 </div>
   )
 }
