4176390e23835d24c7f42c2b941efd464426fab2
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-09-21 19:12:23 -0700

    style updates on footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,6 +6,7 @@ function Home() {
     return (
         <div className="home">
           <h1 className="home_h1">Microblading near me in Corona CA</h1>
+          <h1 className="home_h1">Microblading near me in Lake Elsinore CA</h1>
           <h2 className="home_h2">Best PMU in Riverside, CA</h2>
           <h3 className="home_h2">Best Lip Blushing in Corona, CA</h3>
          <Typewriter 
