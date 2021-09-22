097c68ab8464a409efce8c6fae275a5aa115e5a4
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-09-21 18:15:16 -0700

    calendly styling

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import './Contact.css'
-import emailjs from "emailjs-com";
+import { InlineWidget } from "react-calendly";
+// import emailjs from "emailjs-com"
 
 export default function Contact() {
 
-    function sendEmail(e) {
-      e.preventDefault();
+    // function sendEmail(e) {
+    //   e.preventDefault();
 
-      emailjs.sendForm('Browsjungle', 'template_fv1y4ct', e.target, 'user_CTfSaUyLCF3sUFtRUIZTC')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset();
-    }
+    //   emailjs.sendForm('Browsjungle', 'template_fv1y4ct', e.target, 'user_CTfSaUyLCF3sUFtRUIZTC')
+    //     .then((result) => {
+    //         console.log(result.text);
+    //     }, (error) => {
+    //         console.log(error.text);
+    //     });
+    //     e.target.reset();
+    // }
     return (
         <div className="contact">
-            <form className="contact__form" onSubmit={sendEmail}>
+   <InlineWidget url="https://calendly.com/browsjungle/4hours?primary_color=619c1c" />
+{/* <!-- Calendly inline widget begin -->
+<div class="calendly-inline-widget" data-url="" style="min-width:320px;height:630px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- Calendly inline widget end --> */}
+
+
+            {/* <form className="contact__form" onSubmit={sendEmail}>
             <h1>Contact Us</h1>
             <label> Name </label>
             <input type="text" className="form-control" placeholder="Name" name="name"/>
@@ -31,7 +39,7 @@ export default function Contact() {
         type="submit" >
         - SEND -
       </button>
-            </form>
+            </form> */}
             <h2>Brows Jungle, LLC</h2>
             <p id="contact__id">Email: browsjungle@gmail.com</p>
             <p id="contact__id">Mob.: ‪(747) 224-7451‬</p>
--- src/Pages/pricing/pricing.js
+++ src/Pages/pricing/pricing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from "styled-components";
 import './pricing.css'
 
 
@@ -11,7 +12,9 @@ function pricing() {
     <h3>Lip Blush</h3>
    </div>
    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
-    <h3>$425</h3>
+    <h3>$425
+    <Book a href="https://calendly.com/browsjungle/4hours?primary_color=619c1c" target="_blank">Book Now</Book>
+    </h3>
    </div>
    <div className="pricing__paragraph">Lip Blush tattoo enhances the natural shape of your lips, giving them that boost of vitality and even coloring.</div>
    </div>
@@ -20,7 +23,9 @@ function pricing() {
     <h3>Powder Brows</h3>
    </div>
    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
-    <h3>$425</h3>
+    <h3>$425
+    <Book a href="https://calendly.com/browsjungle/4hours?primary_color=619c1c" target="_blank">Book Now</Book>
+    </h3>
    </div>
    <div className="pricing__paragraph">Powder brows can be done as light as a brow tint or as dark as glam. 
        Recommended for those who want a completely "filled in" brow! Best for ALL skin types. </div>
@@ -30,7 +35,9 @@ function pricing() {
     <h3>Microblading</h3>
    </div>
    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
-    <h3>$425</h3>
+    <h3>$425
+    <Book a href="https://calendly.com/browsjungle/4hours?primary_color=619c1c" target="_blank">Book Now</Book>
+    </h3>
    </div>
    <div className="pricing__paragraph">Microblading is a permanent procedure that mimics the same size of an eyebrow hair stroke through small incisions made with a microblade. 
     The purpose of microblading is to enhance what you naturally have, and the result is natural-looking, trendy eyebrows. *NOT recommended for oily, or mature skin.</div>
@@ -40,7 +47,9 @@ function pricing() {
     <h3>Microblading + Shading (combo)</h3>
    </div>
    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
-    <h3>$425</h3>
+    <h3>$425
+    <Book a href="https://calendly.com/browsjungle/4hours?primary_color=619c1c" target="_blank">Book Now</Book>
+    </h3>
    </div>
    <div className="pricing__paragraph">This technique offers the best of both powdered and hair stroke effects combined together to achieve a fluffy, 
        textured, yet defined brow. </div>
@@ -48,7 +57,7 @@ function pricing() {
    
    <h1 className="pricing__h1">Touch ups</h1>
    <div className="pricing__touchup">
-  <h6><strong>Touch ups also make an essential part of permanent makeup treatment. You can lose 30% to 60% of the color and intensity as the skin heals. 
+  <h6><strong>Touch ups also make an essential part of permanent makeup treatment. You can lose 30% to 40% of the color and intensity as the skin heals. 
       Touch up procedures are to fill, correct, and darken to provide the desired look.</strong></h6>
   </div>
    <div className="row">
@@ -56,8 +65,12 @@ function pricing() {
     <h3>Touch up</h3>
    </div>
    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
-    <h3>$150</h3>
-   </div>
+    <h3>$120
+    <Book a href="https://calendly.com/browsjungle/4hours?primary_color=619c1c" target="_blank">Book Now</Book>
+    </h3>
+
+        
+ </div>
    <div className="pricing__paragraph">1 to 3 months after the first session 
     If you do not come back for a touch up during 3 months period, you will need to pay a full price again.</div>
    </div>
@@ -66,7 +79,9 @@ function pricing() {
     <h3>Annual Touch up</h3>
    </div>
    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
-    <h3>$150</h3>
+    <h3>$150
+    <Book a href="https://calendly.com/browsjungle/4hours?primary_color=619c1c" target="_blank">Book Now</Book> </h3>
+  
    </div>
    <div className="pricing__paragraph">12 months after touch up session</div>
    </div>
@@ -75,7 +90,9 @@ function pricing() {
     <h3>Biannual touch up</h3>
    </div>
    <div className="col-12 col-lg-6 pl-lg-0 pr-lg-6">
-    <h3>$150</h3>
+    <h3>$170
+    <Book a href="https://calendly.com/browsjungle/4hours?primary_color=619c1c" target="_blank">Book Now</Book>
+    </h3>
    </div>
    <div className="pricing__paragraph">24 months after touch up session. If you do not come back for a biannual touch up you will need to pay a full price again. </div>
    </div>
@@ -86,4 +103,30 @@ function pricing() {
     )
 }
 
+
+
+const Book = styled.a`
+  box-shadow: inset 0 0 0 1px #619c1c;
+  color: #619c1c;
+  margin-left: 80px;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: white;
+  text-decoration: none!important;
+  &:hover {
+    cursor: pointer;
+    background-color: #619c1c;
+    color: #white;
+  }
+  @media (max-width: 768px) {
+    margin-right:0px;
+    font-size: 14px;
+    font-weight: 400;
+  }
+`;
 export default pricing
