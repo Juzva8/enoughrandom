43e4e3ffca9ef0f40ab11cf43cc1cfdaab9e339e
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-09-26 22:52:35 -0700

    consent form update

--- src/Pages/ConsentForms/ConsentForms.js
+++ src/Pages/ConsentForms/ConsentForms.js
@@ -14,12 +14,14 @@ function ConsentForms() {
             <h1 className="ConsentForms__h1">AND READ CONSENT FORMS BELLOW</h1>
             
         <div className="ConsentForms__button">
-        <Button href="https://drive.google.com/file/d/1RW4r2GbMt_JhiMOxRWyYrtDbtwV5NXaU/view?usp=sharing" target="_blank" size="lg" block>Before The Procedure (pdf)</Button>
-        <Button href="https://drive.google.com/file/d/1eECmuyZrXa3eW8dCzDBoe9eGe7InzWoz/view?usp=sharing" target="_blank"  size="lg" block>After The Procedure (pdf)</Button>
-        <Button href="https://drive.google.com/file/d/1Xqg050NpUSvxYhWbKowG9AmBUJ-eO9DG/view?usp=sharing" target="_blank" size="lg" block>Main Consent Form (pdf)</Button>
-        <Button href="https://drive.google.com/file/d/1tOA445KrYShPKz8GJ9DpkygeYhV-HdFb/view?usp=sharing" target="_blank" size="lg" block>Medical Consent and Procedure Chart (pdf)</Button>
-        <Button href="https://drive.google.com/file/d/1NY6iY-8O-LtsAZUEv9JT-yNiUND15wl-/view?usp=sharing" target="_blank" size="lg" block>Photo and Liability Release Form (pdf)</Button>
-        <Button href="https://drive.google.com/file/d/1TX0kDJw5yTCrm5zjNkyKx02K3hbVugPB/view?usp=sharing" target="_blank" size="lg" block>Procedure Report (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/1PCWapELgGw6y9ZW7CJ5Egq-5OXbtRuGV/view?usp=sharing" target="_blank" size="lg" block>Before The Procedure BROWS (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/1YyXYIGB8Gj8W93LorYz2U_iUWPjWe-PK/view?usp=sharing" target="_blank"  size="lg" block>After The Procedure BROWS (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/1KWMuoN9QNdrOjV9QYik_ZSYMZHjWhuha/view?usp=sharing" target="_blank" size="lg" block>Before The Procedure LIPS (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/14D0Ew3CIESUeUnbaINS6scU3KIh9at-F/view?usp=sharing" target="_blank"  size="lg" block>After The Procedure LIPS (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/1W1e-_ym4Mx2L0ssPBPuwYiJ96S9eXSyH/view?usp=sharing" target="_blank" size="lg" block>Main Consent Form (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/1zakpDsnXHZea6ff756unhtNfN9_gqFgC/view?usp=sharing" target="_blank" size="lg" block>Medical Consent and Procedure Chart (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/1WJ-F8_RmXtsWZUT6AdklEnjIo1xBcyai/view?usp=sharing" target="_blank" size="lg" block>Photo and Liability Release Form (pdf)</Button>
+        <Button href="https://drive.google.com/file/d/1zlDWps3F8cAqVegLOA_-DhpVQEZ_FkFe/view?usp=sharing" target="_blank" size="lg" block>Procedure Report (pdf)</Button>
         </div>
             </div>
     )
